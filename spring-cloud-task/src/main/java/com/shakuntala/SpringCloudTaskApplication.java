15c4c5370166c4ddb4c81b3dd0f3edadbb3bdacb
CatherineGeorgePres <g.cathy.g@gmail.com>
2018-10-07 23:31:46 +0530

    Fully functional task.

--- spring-cloud-task/src/main/java/com/shakuntala/SpringCloudTaskApplication.java
+++ spring-cloud-task/src/main/java/com/shakuntala/SpringCloudTaskApplication.java
@@ -1,12 +1,38 @@
 package com.shakuntala;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableTask
 public class SpringCloudTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudTaskApplication.class, args);
+	}
+	
+	@Bean
+	public TollProcessingTask tollProcessingTask() {
+		return new TollProcessingTask();
+	}
+	
+	
+	public class TollProcessingTask implements CommandLineRunner{
+
+		@Override
+		public void run(String... args) throws Exception {
+			if(null != args) {
+				String stationID = args[0];
+				String licencePlate = args[1];
+				String timestamp = args[2];
+				System.out.println("Station id is " + stationID + "\n"
+						+"licencePlate is " + licencePlate + "\n"
+						+"timestamp is " + timestamp + "\n");
+			}
+		}
+		
 	}
 }
